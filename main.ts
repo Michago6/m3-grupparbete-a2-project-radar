d956323b02eb66dc97ebc5bf1f30dd544a0f1f12
Michago6 <144434488+Michago6@users.noreply.github.com>
2023-09-13 12:01:20 +0200

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,10 @@
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
+basic.showNumber(3)
+let ymax = 2
+let _119 = 0
+basic.forever(function () {
+    radio.setGroup(116)
+    radio.sendNumber(ymax)
+})
