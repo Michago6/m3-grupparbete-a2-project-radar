cd4a7fb7a251e7143182308a65993c0e2b75c7f8
Michago6 <144434488+Michago6@users.noreply.github.com>
2023-09-13 15:15:28 +0200

    Uppdatera main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 radio.onReceivedNumber(function (receivedNumber) {
 	
 })
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.pause(100)
+    basic.showNumber(0 - ymax - 45)
+    basic.pause(2000)
+    basic.showNumber(3)
+})
+let ymax = 0
 basic.showNumber(3)
-let ymax = 2
+ymax = 2
 let _119 = 0
 basic.forever(function () {
     radio.setGroup(116)
